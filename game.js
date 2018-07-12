5e0a0c33d564252463b8113747310e8a56090547
Maricod <39157704+Maricod@users.noreply.github.com>
2018-07-12 13:08:05 +0300

    Update game.js

--- game.js
+++ game.js
@@ -280,3 +280,13 @@ class Coin extends Actor {
         this.pos = this.getNextPosition(time);
     }
 }
+
+class Player extends Actor {
+    constructor(pos = new Vector(0, 0)) {
+        super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
+    }
+
+    get type() {
+        return 'player';
+    }
+}
